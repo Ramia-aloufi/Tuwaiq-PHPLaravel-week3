8ee85d8c94e3ed4348023d57d225025db284762b
Ramia-aloufi <ramia.aloufi@gmail.com>
2024-01-28 15:42:12 +0300

    auth

--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,18 @@ Route::get('/',[ItemsController::class,'Home'])->name('all-category');
 Route::get('/category/{x}',[ItemsController::class,'Home'])->name('home-category');
 
 //ITEM-GROUP
-route::get('/item-group',[ItemsController::class,'getItemGroup'])->name('item-group');
-route::post('/save-group',[ItemsController::class,'saveItemGroupName'])->name('save-group');
-route::get('/del-group/{x}',[ItemsController::class,'delGroup'])->name('delete-group');
-route::get('/item-group/{x}',[ItemsController::class,'getItemGroup'])->name('edit-group');
-route::post('/update-group',[ItemsController::class,'updateGroup'])->name('update-group');
+route::get('/item-group',[ItemsController::class,'getItemGroup'])->name('item-group')->middleware('auth');
+route::post('/save-group',[ItemsController::class,'saveItemGroupName'])->name('save-group')->middleware('auth');
+route::get('/del-group/{x}',[ItemsController::class,'delGroup'])->name('delete-group')->middleware('auth');
+route::get('/item-group/{x}',[ItemsController::class,'getItemGroup'])->name('edit-group')->middleware('auth');
+route::post('/update-group',[ItemsController::class,'updateGroup'])->name('update-group')->middleware('auth');
 
 //ITEM
-route::get('/items',[ItemsController::class,'getItem'])->name('items');
-route::post('/save-item',[ItemsController::class,'saveItem'])->name('save-item');
-route::get('/del-item/{x}',[ItemsController::class,'delItem'])->name('delete-item');
-route::get('/items/{x}',[ItemsController::class,'getItem'])->name('edit-item');
-route::post('/update-item',[ItemsController::class,'updateItem'])->name('update-item');
+route::get('/items',[ItemsController::class,'getItem'])->name('items')->middleware('auth');
+route::post('/save-item',[ItemsController::class,'saveItem'])->name('save-item')->middleware('auth');
+route::get('/del-item/{x}',[ItemsController::class,'delItem'])->name('delete-item')->middleware('auth');
+route::get('/items/{x}',[ItemsController::class,'getItem'])->name('edit-item')->middleware('auth');
+route::post('/update-item',[ItemsController::class,'updateItem'])->name('update-item')->middleware('auth');
 
 //Cart 
 route::get('/add-to-cart/{x}',[ItemsController::class,'addToCart'])->name('add-to-cart');
