3706f17a7256d813ab01a5369f08c1d91b2c0622
Ramia-aloufi <ramia.aloufi@gmail.com>
2024-02-06 01:21:48 +0300

    add vercel

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public', // Specify the output directory here
+      },
     plugins: [
         laravel({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
-            build: {
-                outDir: 'public', // Specify the output directory here
-              },
+
             refresh: true,
         }),
     ],
